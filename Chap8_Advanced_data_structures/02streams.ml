151f774d384214745fda82f48d0da84886828417
Fang Han <hanfangviolin@gmail.com>
2018-12-22 13:17:41 -0500

    init

--- Chap8_Advanced_data_structures/02streams.ml
+++ Chap8_Advanced_data_structures/02streams.ml
@@ -8,3 +8,36 @@ type 'a stream =
 let rec from n = Cons (n, fun () -> from (n+1))
 
 let nats = from 0
+
+(** in comparison, if without using thunk, stack overflow is gonna happen in the natural number definition 
+    let rec from n = Cons (n, from (n+1));;
+    let nats = from 0;;
+    Stack overflow during evaluation (looping recursion?). *)
+
+(** define operations on streams *)
+(* [hd s] is the head of [s] *)
+let hd =  function
+    | Cons (hd, _) -> hd
+
+let hd_better (Cons (h, _)) = h
+
+(* [tl s] is the tail of [s] *)
+let tl (Cons (_, tf)) = tf ()
+
+(* [take n s] is the list of the first [n] elements of [s] *)
+let rec take n (Cons (hd, tf)) =
+    if n > 0 then  hd::(take (n - 1) (tf ())) 
+    else []
+
+let rec take_better n s = 
+    if n = 0 then []
+    else hd s::take (n-1) (tl s)
+
+(* [drop n s] is all but the first [n] elements of [s] *)
+let rec drop n s = 
+    if n = 0 then s 
+    else drop (n-1) (tl s)
+
+let () = 
+    List.iter (fun x -> print_int x; print_string " ") (take 100 nats);
+    print_newline ()
