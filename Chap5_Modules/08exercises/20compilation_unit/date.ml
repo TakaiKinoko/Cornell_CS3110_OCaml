9568a607e57479476ca57b223a51bc16885db65e
Fang Han <hanfangviolin@gmail.com>
2018-12-27 20:47:57 -0500

    Need to revisit pretty printer

--- Chap5_Modules/08exercises/20compilation_unit/date.ml
+++ Chap5_Modules/08exercises/20compilation_unit/date.ml
@@ -1,5 +1,11 @@
 type date = { month:int; day:int } 
+
 let make_date month day = {month; day}
+
 let get_month d = d.month
+
 let get_day d = d.day
+
 let to_string d = (string_of_int d.month) ^ "/" ^ (string_of_int d.day)
+
+let format f d = Format.fprintf f "@[%s@]@." (to_string d)
--- Chap5_Modules/08exercises/20compilation_unit/date.mli
+++ Chap5_Modules/08exercises/20compilation_unit/date.mli
@@ -1,6 +1,12 @@
 (* type date = { month:int; day:int; }   -- concrete type *)
 type date  (** abstract type *)
+
 val make_date : int -> int -> date
+
 val get_month : date -> int
+
 val get_day : date -> int
+
 val to_string : date -> string
+
+val format : Format.formatter -> date -> unit
